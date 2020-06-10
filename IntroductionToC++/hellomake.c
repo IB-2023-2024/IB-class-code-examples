0f68e44bdebf1022477c7e0229f6f1704024100a
fsande <fsande@ull.es>
2020-06-10 16:09:29 +0100

    Simple make tutorial examples

---
+++ IntroductionToC++/hellomake.c
@@ -0,0 +1,21 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author F. de Sande
+ * @date 8 Jun 2020
+ * @brief hellomake.c Source code for make tutorial
+ * 
+ * @see http://www.cs.colby.edu/maxwell/courses/tutorials/maketutor/
+ */
+
+#include "hellomake.h"
+
+int main() {
+  // call a function in another file
+  myPrintHelloMake();
+
+  return(0);
+}
---
+++ IntroductionToC++/hellomake.h
@@ -0,0 +1,17 @@
+
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author F. de Sande
+ * @date 8 Jun 2020
+ * @brief hellomake.h Header source code for make tutorial
+ *
+ * Example include file
+ * 
+ * @see http://www.cs.colby.edu/maxwell/courses/tutorials/maketutor/
+ */
+
+void myPrintHelloMake(void);
