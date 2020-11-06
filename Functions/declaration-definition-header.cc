5565ab0ad261cc3981c833b539944c0db5c76afe
fsande <fsande@ull.es>
2020-11-06 10:25:01 +0000

    some minor modifications

--- Functions/declaration-definition-header.cc
+++ Functions/declaration-definition-header.cc
@@ -21,19 +21,16 @@
 
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
-vector<int> CreateVectorOfFullSquares(int size) {
-  vector<int> result(size);     // Vector of size `size `
-
-  for (int i = 0; i < size; ++i) {
-    result[i] = i * i;
-  }
-  return result;
-}
+vector<int> CreateVectorOfFullSquares(int size);     // Declaration
 
 int main () {
+  double experiment = sin(0);
+
+  std::cout << experiment;
   auto squares = CreateVectorOfFullSquares(20);
   for (int i = 0; i < 20; ++i) {
     std::cout << squares[i] << ", ";
@@ -43,9 +40,14 @@ int main () {
 }
 
 
+vector<int> CreateVectorOfFullSquares(int size) {
+  vector<int> result(size);     // Vector of size `size `
 
-
-
+  for (int i = 0; i < size; ++i) {
+    result[i] = i * i;
+  }
+  return result;
+}
 
 
 
