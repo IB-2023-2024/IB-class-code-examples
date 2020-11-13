cfaf00174314efb59d3651d99e40ce897ec0ffb5
fsande <fsande@ull.es>
2020-11-13 10:51:17 +0000

    Minor fixings

--- Functions/min.cc
+++ Functions/min.cc
@@ -1,4 +1,16 @@
-
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author F. de Sande
+ * @date 13 Nov 2020
+ * @brief Show the use of the ternary operator
+ *
+ * @see https://www.geeksforgeeks.org/conditional-or-ternary-operator-in-c-c/
+ * 
+ */
 #include <iostream>
 
 int Min(int x, int y) {
@@ -6,12 +18,9 @@ int Min(int x, int y) {
   //   return x;
   // else
   //   return y; 
-  // int my_var{x};
-  // return x < y ? x : y;
-  return;
+  return x < y ? x : y;
 }
 
 int main () {
-
   std::cout << Min(3, 4) << std::endl;
 }
