9f12d744d926f23350565f2ac7022fdc4a24833e
fsande <fsande@ull.es>
2020-11-13 10:49:05 +0000

    Minor fixings

--- Functions/exit_example.cc
+++ Functions/exit_example.cc
@@ -1,18 +1,25 @@
-// A C++ program to show difference 
-// between exit() and _Exit() 
-//
-// https://www.geeksforgeeks.org/exit-vs-_exit-c-cpp/
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author F. de Sande
+ * @date 13 Nov 2020
+ * @brief show difference between exit() and _Exit() 
+ *        exit() aborts the execution of a program
+ *
+ * @see https://www.geeksforgeeks.org/exit-vs-_exit-c-cpp/
+ * 
+ */
 
 #include<bits/stdc++.h> 
 
-using namespace std; 
-  
 void fun(void) { 
-   cout << "Exiting"; 
+  std::cout << "Exiting"; 
 } 
   
 int main() { 
-   atexit(fun); 
-   exit(10); 
+  atexit(fun); 
+  exit(10); 
 } 
-
