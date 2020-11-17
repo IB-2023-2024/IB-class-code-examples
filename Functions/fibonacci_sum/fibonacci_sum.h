5ceace9b98ea7d2df0a3614ee6c26f7acfe17f68
fsande <fsande@ull.es>
2020-11-17 11:28:34 +0000

    Fibonacci sum - separate files

---
+++ Functions/fibonacci_sum/fibonacci_sum.h
@@ -0,0 +1,20 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author F. de Sande
+ * @date 7.nov.2020
+ * @brief Definitions
+ *
+ */
+
+#include <iostream>
+
+const std::string kHelpText = "Este programa calcula la suma de todos los términos pares de la \
+serie de Fibonacci que sean menores que un valor, que el usuario \
+ha de introducir por línea de comandos para la ejecución del programa";
+
+void Usage(int argc, char *argv[]);
+size_t fibonacci_sum(const size_t kLimit);
