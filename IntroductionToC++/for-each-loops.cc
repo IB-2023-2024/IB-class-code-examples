a81a9b6280980a4415f386a805eb1c5c5e457d97
fsande <fsande@ull.es>
2020-11-23 22:46:22 +0000

    for-each-loops

--- IntroductionToC++/for-each-loops.cc
+++ IntroductionToC++/for-each-loops.cc
@@ -39,9 +39,14 @@ int main () {
 	// Fortunately, we can use references for this
 	// And, of course, it’s a good idea to make your reference const if you’re 
 	// intending to use it in a read-only fashion:
-  const int fibonacci[]{ 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 };
-  for (const auto& number : fibonacci) { // type is auto, so number has its type deduced from the fibonacci array
-    std::cout << number << ' ';
+	//
+	//  Rule: In for-each loops element declarations, if your elements are non-fundamental 
+	//  types, use references or const references for performance reasons.
+
+	std::string array[]{ "En", "un", "lugar", "de", "la", "Mancha", "de", "cuyo","nombre", "no","quiero", "..." };
+  for (const auto &element: array) { // element is a const reference to the currently iterated array element
+    std::cout << element << ' ';
   }
+  std::cout << std::endl;
   return 0;
 }
