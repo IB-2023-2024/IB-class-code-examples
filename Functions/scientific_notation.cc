5083cfe83fb732234398566977315574f46f108e
fsande <fsande@ull.es>
2020-11-25 13:01:04 +0000

    Manipulators example

---
+++ Functions/scientific_notation.cc
@@ -0,0 +1,37 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author F. de Sande
+ * @date 23 Jun 2020
+ * @brief I/O Manipulators. setprecision.
+ * If fixed or scientific notation is used, precision determines how many decimal places in the fraction is displayed. 
+ * Note that if the precision is less than the number of significant digits, the number will be rounded.
+ *
+ * @see http://www.cplusplus.com/reference/library/manipulators/
+ * @see https://www.learncpp.com/cpp-tutorial/183-output-with-ostream-and-ios/
+ */
+
+#include <iostream>
+#include <iomanip>
+
+const double kValue = 123.456;
+
+int main () {
+  std::cout << std::fixed << std::endl;
+  std::cout << std::setprecision(3) << kValue << std::endl;
+  std::cout << std::setprecision(4) << kValue << std::endl;
+  std::cout << std::setprecision(5) << kValue << std::endl;
+  std::cout << std::setprecision(6) << kValue << std::endl;
+  std::cout << std::setprecision(7) << kValue << std::endl;
+
+  std::cout << std::scientific << std::endl;
+  std::cout << std::setprecision(3) << kValue << std::endl;
+  std::cout << std::setprecision(4) << kValue << std::endl;
+  std::cout << std::setprecision(5) << kValue << std::endl;
+  std::cout << std::setprecision(6) << kValue << std::endl;
+  std::cout << std::setprecision(7) << kValue << std::endl;
+  return 0;
+}
---
+++ Functions/setw.cc
@@ -0,0 +1,24 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author F. de Sande
+ * @date 23 Jun 2020
+ * @brief I/O Manipulators
+ *
+ * @see http://www.cplusplus.com/reference/library/manipulators/
+ * @see https://www.learncpp.com/cpp-tutorial/182-input-with-istream/
+ */
+
+#include <iostream>
+#include <iomanip>
+
+int main () {
+  std::string input_string;
+  std::cout << "Introduzca una cadena de texto (solo leerá 9 caracteres): ";
+  std::cin >> std::setw(5) >> input_string;
+  std::cout << "Cadena introducida: " << input_string << std::endl;
+  return 0;
+}
