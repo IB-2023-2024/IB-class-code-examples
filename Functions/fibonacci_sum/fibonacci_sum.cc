64348ff1edfc969b0d79cc761843a91d10c30ea9
fsande <fsande@ull.es>
2020-12-09 09:48:25 +0000

    Bug fixed

--- Functions/fibonacci_sum/fibonacci_sum.cc
+++ Functions/fibonacci_sum/fibonacci_sum.cc
@@ -53,17 +53,20 @@ size_t fibonacci_sum(const size_t kLimit) {
   size_t second_to_last{0},  // Second to last term
            last{1},          // Last term generated
            new_term;         // New term of the serie
-  size_t long sum{0};        // Accumulated sum of the terms
+  size_t sum{0};        // Accumulated sum of the terms
 
   do {
     new_term = last + second_to_last;
-    if (new_term % 2 == 0) {
+    if ((new_term % 2 == 0) && (new_term < kLimit)) {
       sum += new_term;
+
     }
     // Uncomment for debug: print each new term
     // std::cout << "Term: " << new_term << std::endl;
     second_to_last = last;
     last = new_term;
   } while (new_term < kLimit);
-	return sum;
+
+
+  return sum;
 }
