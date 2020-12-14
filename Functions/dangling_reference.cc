347949771edc226d59a281fdec0e3a1347f577ba
fsande <fsande@ull.es>
2020-12-14 11:44:35 +0000

    minor fixings

--- Functions/dangling_reference.cc
+++ Functions/dangling_reference.cc
@@ -10,17 +10,21 @@
  *        It is illegal to access a reference to an object that has gone out of scope or been otherwise destroyed. 
  *        Such a reference is said to be dangling ("colgada") since it no longer refers to a valid object.
  *
- *        In this example, the local variable x goes out of scope when getX returns. 
+ *        In this example, the local variable x goes out of scope when GetX returns. 
+ *        GetX is returning a reference to a value that will be destroyed when the function returns.
  *        Therefore r is a dangling reference. 
+ *        This would mean the caller receives a reference to garbage. 
+ *        Fortunately, your compiler will probably give you a warning or error if you try to do this.
  *        This program has undefined behavior, although it may appear to work and print 42 in some cases.
  *
  *        The compiler generates a warning message:
  *        dangling_reference.cc: In function ‘int& GetX()’:
- *        dangling_reference.cc:29:10: warning: reference to local variable ‘x’ returned [-Wreturn-local-addr]
- *           30 |   return x;
+ *        dangling_reference.cc::10: warning: reference to local variable ‘x’ returned [-Wreturn-local-addr]
+ *              |   return x;
  *              |          ^
  *
  * @see https://www.learncpp.com/cpp-tutorial/74a-returning-values-by-value-reference-and-address/
+ * @see https://stackoverflow.com/questions/46011510/what-is-a-dangling-reference
  */
 
 #include <iostream>
---
+++ Functions/return_by_reference.cc
@@ -0,0 +1,43 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author F. de Sande
+ * @date Dec 2020
+ * @brief Return by reference
+ *        Values returned by reference must be variables (you should not return a reference 
+ *        to a literal or an expression that resolves to a temporary value, as those will go 
+ *        out of scope at the end of the function and you’ll end up returning a dangling reference).
+ *        When a variable is returned by reference, a reference to the variable is passed back to the caller.
+ *        The caller can then use this reference to continue modifying the variable, which can be useful at times.
+ *        Return by reference is also fast, which can be useful when returning structs and classes.
+ *        However, just like return by address, you should not return local variables by reference
+ *
+ *        Return by reference is typically used to return arguments passed by reference to the function back to the caller. 
+ *        In this example, we return (by reference) an element of an array that was passed to our function by reference:
+ *        
+ * @see https://www.learncpp.com/cpp-tutorial/74a-returning-values-by-value-reference-and-address/
+ */
+
+#include <array>
+#include <iostream>
+ 
+// Returns a reference to the index element of array
+int& getElement(std::array<int, 25> &array, int index) {
+  // we know that array[index] will not be destroyed when we return to the caller (since the caller passed in the array in the first place!)
+  // so it's okay to return it by reference
+  return array[index];
+}
+ 
+int main() {
+  std::array<int, 25> array;
+
+  // Set the element of array with index 10 to the value 5
+  getElement(array, 10) = 5;
+  std::cout << array[10] << '\n';
+ 
+  return 0;
+}
+
