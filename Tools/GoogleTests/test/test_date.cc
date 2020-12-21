b097e0c013f8322853a721f9b0bb2bc18c8486c8
fsande <fsande@ull.es>
2020-12-21 10:35:20 +0000

    Date tests added

--- Tools/GoogleTests/test/test_date.cc
+++ Tools/GoogleTests/test/test_date.cc
@@ -1,28 +1,26 @@
 #include <gtest/gtest.h>
 
-#include "multiply.h"
+#include "date.h"
  
-TEST(DateTest, twoValues){
+TEST(DateTest, greater){
+    Date d1{2020, 12, 1};
+    Date d2{2020, 12, 2};
+    Date d3{2020, 11, 1};
+
+    EXPECT_EQ(true, d2 > d1);
+    EXPECT_EQ(false, d3 > d1);
+}
+
+TEST(DateTest, equal){
     Date d1{2020, 12, 1};
     Date d2{2020, 12, 1};
 
-    EXPECT_EQ(20, multiply.TwoValues(x, y));
-    EXPECT_EQ(6, multiply.TwoValues(2, 3));
-}
+    Date d3{2020, 12, 2};
+    Date d4{2020, 11, 1};
+    Date d5{2019, 12, 1};
 
-
-
-  Date today{2020, 12, 1};  // initialize using parameterized constructor (C++11)
-  const Date date1{};               // initialize using default constructor
-  const Date date2{2020, 1, 18}; 
-  const Date date3{2020, 12, 18}; 
-	if (date3 > date2) {
-	  std::cout << date3 << " > " << date2 << std::endl;
-	}
-	else
-	{
-	  std::cout << date3 << " <= " << date2 << std::endl;
-	}
-	std::cout << "Date: " << date2 << std::endl;
-  return 0;
+    EXPECT_EQ(true, d1 == d2);
+    EXPECT_EQ(false, d1 == d3);
+    EXPECT_EQ(false, d1 == d4);
+    EXPECT_EQ(false, d1 == d5);
 }
