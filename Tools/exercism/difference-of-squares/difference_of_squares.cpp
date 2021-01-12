dddb48ee625d8f5d40744dbdd737eb6d912d59d1
fsande <fsande@ull.es>
2021-01-12 10:34:45 +0000

    Changes after problem-class session

--- Tools/exercism/difference-of-squares/difference_of_squares.cpp
+++ Tools/exercism/difference-of-squares/difference_of_squares.cpp
@@ -2,4 +2,26 @@
 
 namespace difference_of_squares {
 
+int square_of_sum(int n) {
+  int sum{0};
+
+  for (int i = 1; i <= n; ++i) {
+    sum += i;
+  }
+  return sum * sum;
+}
+
+int sum_of_squares(int n) {
+  int sum{0};
+
+  for (int i = 1; i <= n; ++i) {
+    sum += i * i;
+  }
+  return sum;
+}
+
+int difference(int n) {
+  return (square_of_sum(n) - sum_of_squares(n));
+}
+
 }  // namespace difference_of_squares
--- Tools/exercism/difference-of-squares/difference_of_squares.h
+++ Tools/exercism/difference-of-squares/difference_of_squares.h
@@ -3,6 +3,10 @@
 
 namespace difference_of_squares {
 
+int square_of_sum(int n);
+int sum_of_squares(int n);
+int difference(int n);
+
 }  // namespace difference_of_squares
 
 #endif // DIFFERENCE_OF_SQUARES_H
