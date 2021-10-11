b5b31532960ad38878e50d2c9b0b35357c584651
F. de Sande <fsande@ull.es>
2021-10-11 12:36:23 +0000

    Iostream code example

---
+++ IntroductionToC++/iostream_example.cc
@@ -0,0 +1,23 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author F. de Sande
+ * @date 11 Oct 2020
+ * @brief Simple I/O streams usage
+ *
+ */
+
+#include <iostream>
+
+int main () {
+  int some_number;
+
+  std::cout << "Input a number: ";
+  std::cin >> some_number;
+  std::cout << "number = " << some_number << std::endl;
+  std::cerr << "boring error message " << std::endl;
+  return 0;
+}
