e156a1eefa03517c187954ec74b35622c448d136
fsande <fsande@ull.es>
2021-10-22 13:52:53 +0100

    New example to explain pre-post increment

---
+++ IntroductionToC++/pre-post_increment.cc
@@ -0,0 +1,31 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author F. de Sande
+ * @date Oct 21 2021
+ * @brief Difference between pre-increment and post-increment
+ *        Best practice: Strongly favor the prefix version of the increment and decrement operators, 
+ *        as they are generally more performant, and you’re less likely to run into strange issues with them.
+ * @see https://www.learncpp.com/cpp-tutorial/increment-decrement-operators-and-side-effects/
+ */
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main () {
+  int variable{5};
+  int another_var= variable++; // variable is incremented to 6, copy of original variable 
+                               // is evaluated to the value 5, and 5 is assigned to another_var
+  std::cout << variable << ' ' << another_var << std::endl;
+
+  variable = 10;
+  another_var = ++variable;
+  std::cout << variable << ' ' << another_var << std::endl;
+
+  return 0;
+}
