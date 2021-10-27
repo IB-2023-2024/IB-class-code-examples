ca66390a14e4971be3bcadc71342aa865383492f
fsande <fsande@ull.es>
2021-10-27 16:48:03 +0100

    Including always braces

--- IntroductionToC++/const_with_references.cc
+++ IntroductionToC++/const_with_references.cc
@@ -21,19 +21,23 @@
 
 int main () {
   int number{42}; 
-  int another_number{7};
+  int another_number{4};
   int& ref{number};
   const int& kRef{number};
 
   ref = 0;
   std::cout << ref << " " << number << " " << kRef << std::endl;
 
-  number = 24;
+  number = 2;
   std::cout << ref << " " << number << " " << kRef << std::endl;
 
   ref = another_number;        // You can change the value of ref
+  std::cout << ref << " " << number << " " << kRef << std::endl;
+
+  number = 8;
+  std::cout << ref << " " << number << " " << kRef << std::endl;
+
   // kRef = another_number;    // But YOU CAN'T CHANGE (it's const) the value of kRef
-  std::cout << ref << " " << another_number << " " << kRef << std::endl;
 
   return 0;
 }
--- IntroductionToC++/if_statement.cc
+++ IntroductionToC++/if_statement.cc
@@ -19,22 +19,30 @@ int main () {
   const int kNumber{100};  // Play modifying this value
 
   // Version 1
-  if (kNumber > 0)
+  if (kNumber > 0) {
     std::cout << "V1 The number is positive" << std::endl;
+  }
 
   // Version 2: if-else
-  if (kNumber > 0)
+  if (kNumber > 0) {
     std::cout << "V2 The number is positive" << std::endl;
-  else 
-    if (kNumber < 0)
+  }
+  else {
+    if (kNumber < 0) {
       std::cout << "V2 The number is negative" << std::endl;
+    }
+  }
 
   // Version 3: if-else-if
-  if (kNumber > 0)
+  if (kNumber > 0) {
     std::cout << "V3 The number is positive" << std::endl;
-  else 
-    if (kNumber < 0)
+  }
+  else {
+    if (kNumber < 0) {
       std::cout << "V3 The number is negative" << std::endl;
-    else
+    }
+    else {
       std::cout << "V3 The number is 0";
+    }
+  }
 }
