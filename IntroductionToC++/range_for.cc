5230e6d460d6fe0d6c04ff1987514ac23bfbe401
fsande <fsande@ull.es>
2022-10-16 19:13:13 +0100

    using namespace deleted

--- IntroductionToC++/range_for.cc
+++ IntroductionToC++/range_for.cc
@@ -16,10 +16,8 @@
 #include <vector>
 #include <iostream>
 
-using namespace std;
-
 int main () {
-  vector<int> fibonacci = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89};
+  std::vector<int> fibonacci = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89};
 
   for (int number: fibonacci) { // iterate over array fibonacci 
     std::cout << number << ' '; 
