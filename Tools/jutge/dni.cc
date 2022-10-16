9ed6d5f85657a9e109cd1ffbba14eec787af593e
fsande <fsande@ull.es>
2022-10-16 20:16:31 +0100

    Header comment updated

--- Tools/jutge/dni.cc
+++ Tools/jutge/dni.cc
@@ -7,7 +7,9 @@
  * @author F. de Sande
  * @date Oct 27 2021
  * @brief Computes the DNI letter from the number
+ *        Jutge problem P97139
  *        The program takes directly the input and prints the output
+ * @see https://jutge.org/problems/P97139
  * @see http://www.interior.gob.es/web/servicios-al-ciudadano/dni/calculo-del-digito-de-control-del-nif-nie
  */
 
