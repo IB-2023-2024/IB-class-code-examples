6c312d7418d590149b7c96435b3b76bbd11b5eb6
F de Sande <fsande@ull.es>
2022-12-08 20:30:58 +0000

    Code improvement

--- object_relationship/calling_base_function.cc
+++ object_relationship/calling_base_function.cc
@@ -7,12 +7,12 @@
  * @author F.de Sande
  * @date 05 Jan 2021
  * @brief Illustrates calling a base function from a derived class
- *        When derived.identify() is called, the compiler looks to 
- *        see if function identify() has been defined in the Derived class. 
+ *        When derived.Identify() is called, the compiler looks to 
+ *        see if function Identify() has been defined in the Derived class. 
  *        It hasn’t. 
  *        Then it starts looking in the inherited classes (which in this case is Base). 
- *        Base has defined an identify() function, so it uses that one. 
- *        In other words, Base::identify() was used because Derived::identify() doesn’t exist.
+ *        Base has defined an Identify() function, so it uses that one. 
+ *        In other words, Base::Identify() was used because Derived::Identify() doesn’t exist.
  *        This means that if the behavior provided by a base class is sufficient, 
  *        we can simply use the base class behavior.
  *
@@ -26,7 +26,7 @@ class Base {
   Base(int value) : value_(value) {
   }
 
-  void identify() { std::cout << "I am a Base\n"; }
+  void Identify() { std::cout << "I am a Base\n"; }
  protected:
   int value_;
 };
@@ -39,10 +39,10 @@ class Derived: public Base {
 
 int main() {
   Base base(5);
-  base.identify();
+  base.Identify();
 
   Derived derived(7);
-  derived.identify();
+  derived.Identify();
 
   return 0;
 }
