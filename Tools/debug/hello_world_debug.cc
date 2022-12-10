25c54bd078a8d1f7b7eec93556a5ca317b44687a
fsande <fsande@ull.es>
2022-12-10 21:50:57 +0000

    Comments added

--- Tools/debug/hello_world_debug.cc
+++ Tools/debug/hello_world_debug.cc
@@ -1,3 +1,14 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author MS Visual Studio
+ * @brief A simple program to begin working with VSC
+ * @see https://code.visualstudio.com/docs/cpp/config-linux
+ */
+
 #include <iostream>
 #include <vector>
 #include <string>
