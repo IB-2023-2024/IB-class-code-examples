3b8b510b71f3e5acde021b118453c937afc70b8e
cexposit <cexposit@ull.edu.es>
2023-10-24 08:50:47 +0100

    minor changes

--- IntroductionToC++/11_fibonacci.cc
+++ IntroductionToC++/11_fibonacci.cc
@@ -8,7 +8,6 @@
  * @date 23 October 2023
  * @brief Programa que imprime los N primeros términos de la Serie de Fibonacci.
  *        Los primeros términos de la serie son: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233,...
- *
  */
 
 #include <iostream>
