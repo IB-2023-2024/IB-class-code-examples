9e4142824ff60a5e65a550c0bfba4bfe3826586c
fsande <fsande@ull.es>
2023-11-16 20:38:12 +0000

    Minor fixings

--- operator_overloading/point_output_print.cc
+++ operator_overloading/point_output_print.cc
@@ -5,7 +5,7 @@
  * Informática Básica
  *
  * @author F. de Sande
- * @date 10 Dec 2020
+ * @date Dec 10 2020
  * @brief Overloading operator<<
  *        In this approach we write a Print() method (member function) that prints the object
  *
@@ -16,17 +16,17 @@
 
 class Point {
  public:
-  Point(double x = 0.0, double y = 0.0, double z = 0.0) : x_{x}, y_{y}, z_{z} { }
-  double getX() const { return x_; }
-  double getY() const { return y_; }
-  double getZ() const { return z_; }
+  Point(double x = 0.0, double y = 0.0, double z = 0.0) : x_coordinate_{x}, y_coordinate_{y}, z_coordinate_{z} { }
+  double getX() const { return x_coordinate_; }
+  double getY() const { return y_coordinate_; }
+  double getZ() const { return z_coordinate_; }
   void Print() const {
-    std::cout << "Point(" << x_ << ", " << y_ << ", " << z_ << ')';
+    std::cout << "Point(" << x_coordinate_ << ", " << y_coordinate_ << ", " << z_coordinate_ << ')';
   }
  private:
-  double x_;
-  double y_;
-  double z_;
+  double x_coordinate_;
+  double y_coordinate_;
+  double z_coordinate_;
 };
 
 int main() {
